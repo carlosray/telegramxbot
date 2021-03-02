beccddae0b0a188e6c9a9445800d973cb0517135
carlosray <carlos.ray@mail.ru>
2021-03-02 10:42:32 +0300

    sorry service bug fix

--- src/main/java/com/example/telegramxbot/bot/service/SorryService.java
+++ src/main/java/com/example/telegramxbot/bot/service/SorryService.java
@@ -22,13 +22,13 @@ public class SorryService {
             "Sorry boys",
             "打擾一下",
             "Вибачте мене",
-            "Си сэ упыс"};
+            "Къысфэгъэгъу"};
 
     public boolean isNeedSorry(Message message, String botUsername) {
         return Optional.of(message.getText())
                 .filter(StringUtils::isNotBlank)
                 .map(String::toLowerCase)
-                .filter(text -> StringUtils.containsAny("извин", "извен"))
+                .filter(text -> StringUtils.containsAny(text,"извин", "извен", "проси проще"))
                 .flatMap(text -> Optional.ofNullable(message.getReplyToMessage())
                         .map(Message::getFrom)
                         .filter(User::getIsBot)
